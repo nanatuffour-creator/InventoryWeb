a5c875b7ae26a6d098c6936ff1f38309f1f10f74
nanatuffour-creator <tuffour_n@yahoo.com>
2025-12-12 17:17:49 +0000

    Purchases amended

--- Controllers/PurchaseController.cs
+++ Controllers/PurchaseController.cs
@@ -10,13 +10,13 @@ namespace InventoryWeb.Controllers
     [ApiController]
     public class PurchaseController(PurchaseService service) : ControllerBase
     {
-        private readonly PurchaseService _service  = service;
+        private readonly PurchaseService _service = service;
 
         [HttpPost("add")]
         public IActionResult AddPurchase([FromBody] PurchaseDto dto)
         {
             _service.Add(dto);
-            return Ok(new {message="Purchase Added"});
+            return Ok(new { message = "Purchase Added" });
         }
 
         [HttpGet("all")]
@@ -24,6 +24,45 @@ namespace InventoryWeb.Controllers
         {
             var all = _service.GetPurchase();
             return Ok(all);
+        }
+
+        [HttpGet("completedpurchase")]
+        public IActionResult GetAllPurchasesCompleted()
+        {
+            var all = _service.GetPurchasesCompleted();
+            return Ok(all);
+        }
+
+        [HttpGet("pendingpurchase")]
+        public IActionResult GetAllPurchasesPending()
+        {
+            var all = _service.GetPurchasesPending();
+            return Ok(all);
+        }
+
+        [HttpGet("delayedpurchase")]
+        public IActionResult GetAllPurchasesDelayed()
+        {
+            var all = _service.GetPurchasesDelayed();
+            return Ok(all);
+        }
+
+        [HttpGet("total")]
+        public Decimal GetTotalPurchase()
+        {
+            return _service.GetTotalPurchase();
+        }
+
+        [HttpGet("pending")]
+        public int GetPendingPurchases()
+        {
+            return _service.GetPendingOrders();
+        }
+
+        [HttpGet("completed")]
+        public int GetCompletedPurchases()
+        {
+            return _service.GetCompletedOrders();
         }
     }
 }
--- Services/PurchaseService.cs
+++ Services/PurchaseService.cs
@@ -64,5 +64,109 @@ public class PurchaseService(DatabaseContext context)
         return [.. query];
     }
 
+    public IEnumerable<GetPurchaseDto> GetPurchasesCompleted()
+    {
+        var query = from u in _context.Purchases
+                    join s in _context.Supplier on u.Id equals s.Id
+                    where u.Stat == Enum.Status.Completed
+                    select new GetPurchaseDto
+                    {
+                        PurchaseId = u.PurchaseId,
+                        SupplierName = s.Name,
+                        Amount = u.Amount,
+                        Date = u.Date,
+                        Stat = u.Stat,
+                        PurchaseOrders = (
+                            from m in _context.PurchaseOrder
+                            join g in _context.Products on m.ProductId equals g.ProductId
+                            where m.PurchaseId == u.PurchaseId
+                            select new GetPurchaseOrdersDto
+                            {
+                                ProductName = g.ProductName,
+                                CostPrice = m.CostPrice,
+                                Quantity = m.Quantity
+                            }
+                        ).ToList()
+                    };
+
+        return [.. query];
+    }
+
+    public IEnumerable<GetPurchaseDto> GetPurchasesPending()
+    {
+        var query = from u in _context.Purchases
+                    join s in _context.Supplier on u.Id equals s.Id
+                    where u.Stat == Enum.Status.Pending
+                    select new GetPurchaseDto
+                    {
+                        PurchaseId = u.PurchaseId,
+                        SupplierName = s.Name,
+                        Amount = u.Amount,
+                        Date = u.Date,
+                        Stat = u.Stat,
+                        PurchaseOrders = (
+                            from m in _context.PurchaseOrder
+                            join g in _context.Products on m.ProductId equals g.ProductId
+                            where m.PurchaseId == u.PurchaseId
+                            select new GetPurchaseOrdersDto
+                            {
+                                ProductName = g.ProductName,
+                                CostPrice = m.CostPrice,
+                                Quantity = m.Quantity
+                            }
+                        ).ToList()
+                    };
+
+        return [.. query];
+    }
+
+    public IEnumerable<GetPurchaseDto> GetPurchasesDelayed()
+    {
+        var query = from u in _context.Purchases
+                    join s in _context.Supplier on u.Id equals s.Id
+                    where u.Stat == Enum.Status.Delayed
+                    select new GetPurchaseDto
+                    {
+                        PurchaseId = u.PurchaseId,
+                        SupplierName = s.Name,
+                        Amount = u.Amount,
+                        Date = u.Date,
+                        Stat = u.Stat,
+                        PurchaseOrders = (
+                            from m in _context.PurchaseOrder
+                            join g in _context.Products on m.ProductId equals g.ProductId
+                            where m.PurchaseId == u.PurchaseId
+                            select new GetPurchaseOrdersDto
+                            {
+                                ProductName = g.ProductName,
+                                CostPrice = m.CostPrice,
+                                Quantity = m.Quantity
+                            }
+                        ).ToList()
+                    };
+
+        return [.. query];
+    }
+
+    public decimal GetTotalPurchase()
+    {
+        var total = _context.Purchases.Sum(i => i.Amount);
+        return total;
+    }
+
+    public int GetPendingOrders()
+    {
+        int pending = _context.Purchases.Count(i => i.Stat == Enum.Status.Pending);
+        return pending;
+    }
+
+    public int GetCompletedOrders()
+    {
+        DateOnly today = DateOnly.FromDateTime(DateTime.Today);
+        int completed = _context.Purchases
+                        .Where(e => e.Date == today)
+                        .Count(i => i.Stat == Enum.Status.Completed);
+        return completed;
+    }
 
 }
